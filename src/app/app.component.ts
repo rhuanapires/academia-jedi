d5e5839ac594acc12953feb64f557643b3b241dc
Rhuana Pires <rhuanapires@gmail.com>
2019-08-26 16:07:57 -0300

    Adding title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: '',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "title-jedi",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-
+  titulo: string = "Academia Jedi";
 }
