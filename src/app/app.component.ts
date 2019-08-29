b9c22552a0044cc4519f14d5a9389fc66def596a
Rhuana Pires <rhuanapires@gmail.com>
2019-08-28 22:15:56 -0300

    Changing Selector's name to Jedi as requested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "title-jedi",
+  selector: "jedi",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
